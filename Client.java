d8a93373579404db225a667e3907180a1417e151
Supawish34 <supawish.kj@gmail.com>
2019-05-07 20:29:15 +0700

    Last

--- Client.java
+++ Client.java
@@ -8,15 +8,15 @@ import java.net.Socket;
 import java.util.Scanner;
 public class Client {
 
- //   private static int LP = 5;
+    private static int LP = 5;
     private static int InitPort = 6789;
-//	private static int IsWin = 0;
-//  private static int IsLose = 0;
-//	private static int MissCount = 0;
+	private static int IsWin = 0;
+    private static int IsLose = 0;
+	private static int MissCount = 0;
     private static String HOST = "127.0.0.1";
     private static Socket socket = null;
-//    private static String SecretWord = "";
-//    private static String Miss = "";
+    private static String SecretWord = "";
+    private static String Miss = "";
 	private static ObjectOutputStream SocketOutput = null;
     private static ObjectInputStream SocketInput = null;
 	private static String Human[] = {     "|=====\n" +
@@ -82,7 +82,48 @@ public class Client {
             socket = new Socket(HOST, Integer.parseInt(newPort));
             SocketOutput = new ObjectOutputStream(socket.getOutputStream());
             SocketInput = new ObjectInputStream(socket.getInputStream());
+  
+            GetStatus();
 
+            while (IsWin == 0 && IsLose ==0) {
+
+
+                
+                System.out.println("**********************************************\n");
+                System.out.println("You have Life Point : " + (LP - MissCount));
+                System.out.println("Word:\t" + SecretWord);
+                System.out.println("Misses: " + Miss);
+				System.out.println("\n ");
+
+				System.out.println(Human[LP-MissCount]);
+
+                System.out.print("\nGuess: ");
+
+                
+                sendUserInput(keyboard.nextLine());
+
+               
+                GetStatus();
+            }
+
+            System.out.println("**********************************************");
+
+            if (IsWin==1){
+                System.out.println( "Winner" );
+                System.out.println( "SecretWord was : " + SecretWord );
+
+            }
+            else if (IsLose ==1) {
+                String answer = GetAnswer();
+                System.out.println("Game Over");
+                System.out.println("SecretWord was : " + answer);
+				System.out.println("\n ");
+
+				System.out.println(Human[0]);
+            }
+
+    
+            SocketOutput.writeObject("Exit");
             
          
         
@@ -96,6 +137,50 @@ public class Client {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+	private static void sendUserInput(String s){
+        try {
+            SocketOutput.writeObject("send:"+s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+	 private static void GetStatus(){
+        try {
+            SocketOutput.writeObject("GetStatus");
+            String input = (String) SocketInput.readObject();
+            String[] res = input.split("@");  
+
+           
+            SecretWord = res[0];
+            MissCount = Integer.parseInt(res[1]);
+            Miss = res[2];
+            IsWin = Integer.parseInt(res[3]);
+            IsLose = Integer.parseInt(res[4]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static String GetAnswer(){
+        try {
+            SocketOutput.writeObject("GetAnswer");
+            String input = (String) SocketInput.readObject();
+            return input;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
 
